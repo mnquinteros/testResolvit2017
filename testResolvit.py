15a2fc3f99bc834f191874c82352c8e6c2f437d7
marcelo <mn_quinteros@yahoo.com.ar>
2017-12-01 01:19:40 -0300

    fix variable name

--- testResolvit.py
+++ testResolvit.py
@@ -48,13 +48,13 @@ class WordFrequencyAnalyzer():
 		result = {}
 
 		for sentenceIdx , word in sentenceTable:
-			t = res.get( word )
+			t = result.get( word )
 			if not t:
 				t = ( 0, set() )
 			count, setIdx = t
 			setIdx.add(sentenceIdx) 
 			countAndIdxUpdated = ( count + 1 , setIdx )
-			res.update( [ ( word , countAndIdxUpdated ) ] )
+			result.update( [ ( word , countAndIdxUpdated ) ] )
 
 		return result
 
@@ -81,7 +81,7 @@ class WordFrequencyAnalyzer():
 ###########################
 
 if __name__ == '__main__':
-    text = "Take this paragraph of text and return an alphabetized list of ALL unique words.  A unique word is any form of a word often communicated with essentially the same meaning. For example, fish and fishes could be defined as a unique word by using their stem fish. For each unique word found in this entire paragraph, determine the how many times the word appears in total. Also, provide an analysis of what sentence index position or positions the word is found. The following words should not be included in your analysis or result set: \"a\", \"the\", \"and\", \"of\", \"in\", \"be\", \"also\" and \"as\".  Your final result MUST be displayed in a readable console output in the same format as the JSON sample object shown below."
+	text = "Take this paragraph of text and return an alphabetized list of ALL unique words.  A unique word is any form of a word often communicated with essentially the same meaning. For example, fish and fishes could be defined as a unique word by using their stem fish. For each unique word found in this entire paragraph, determine the how many times the word appears in total. Also, provide an analysis of what sentence index position or positions the word is found. The following words should not be included in your analysis or result set: \"a\", \"the\", \"and\", \"of\", \"in\", \"be\", \"also\" and \"as\".  Your final result MUST be displayed in a readable console output in the same format as the JSON sample object shown below."
 	wordAnalyzer = WordFrequencyAnalyzer()
 	result = wordAnalyzer.analyzeTextAndGetStats(text)
 	print(result)
